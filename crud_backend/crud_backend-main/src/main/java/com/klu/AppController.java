1feb9b270a12aebf75bd81a52f16958585ff73fd
M. AJIT PATRA <2300031377@kluniversity.in>
2025-09-22 00:01:06 +0530

    added import of restcontroller

--- crud_backend/crud_backend-main/src/main/java/com/klu/AppController.java
+++ crud_backend/crud_backend-main/src/main/java/com/klu/AppController.java
@@ -2,6 +2,7 @@ package com.klu;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
